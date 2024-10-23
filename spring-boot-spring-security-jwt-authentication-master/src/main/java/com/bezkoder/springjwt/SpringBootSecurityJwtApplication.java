07565a3183db81d6fdd2550f093bada3efe74316
PradeepEswaran7 <145675122+PradeepEswaran7@users.noreply.github.com>
2024-10-23 18:22:28 +0530

    Add files via upload

---
+++ spring-boot-spring-security-jwt-authentication-master/src/main/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.bezkoder.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // This annotation will help to start the application.
+public class SpringBootSecurityJwtApplication {
+
+	public static void main(String[] args) { // main method 
+    SpringApplication.run(SpringBootSecurityJwtApplication.class, args); // to run the application
+	}
+
+}
